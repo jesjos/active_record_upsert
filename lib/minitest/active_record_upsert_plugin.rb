2fd49eb55c07f8a5f637fa42cc58af5ad1b25f06
Olle Jonsson <olle.jonsson@gmail.com>
2016-04-20 08:47:36 +0200

    Document Minitest patch plugin

--- lib/minitest/active_record_upsert_plugin.rb
+++ lib/minitest/active_record_upsert_plugin.rb
@@ -1,8 +1,8 @@
 module Minitest
+  # In order to make Rails' minitest railtie understand the :spec Rake task's
+  # generated --pattern ARGV option, this little plugin adds a noop option
+  # for it - this avoids errors.
   def self.plugin_active_record_upsert_options(opts, _options)
-    opts.on('--pattern') do
-      $stderr.puts 'Yeah, man, --pattern is from RSpec, ignore it.'
-    end
+    opts.on('--pattern') # --pattern is from RSpec, ignore it.
   end
 end
-
