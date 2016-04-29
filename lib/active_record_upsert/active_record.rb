3a7d1b269948e6ce3ea9578d49f803c584d8f045
Jesper Josefsson <jesper.josefsson@gmail.com>
2016-04-29 10:31:22 +0200

    Avoid loading pg gem by mistake

--- lib/active_record_upsert/active_record.rb
+++ lib/active_record_upsert/active_record.rb
@@ -1,4 +1,6 @@
-require 'active_record/connection_adapters/postgresql_adapter'
+unless defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  require 'active_record/connection_adapters/postgresql_adapter'
+end
 
 Dir.glob(File.join(__dir__, 'active_record/**/*.rb')) do |f|
   require f
