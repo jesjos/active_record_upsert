98fcf11317c96176de0fbb5bd140f21c34fc3cc2
Olle Jonsson <olle.jonsson@gmail.com>
2016-10-16 10:15:02 +0200

    Re-introduce the require psql adapter line

--- lib/active_record_upsert/active_record.rb
+++ lib/active_record_upsert/active_record.rb
@@ -1,3 +1,6 @@
+unless defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  require 'active_record/connection_adapters/postgresql_adapter'
+end
 Dir.glob(File.join(__dir__, 'active_record/**/*.rb')) do |f|
   require f
 end
