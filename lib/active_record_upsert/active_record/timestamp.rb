b6994037e587a9e3241276020f77bd230460882b
Kevin Poorman <codefriar@users.noreply.github.com>
2017-06-09 14:39:47 -0400

    ugh

--- lib/active_record_upsert/active_record/timestamp.rb
+++ lib/active_record_upsert/active_record/timestamp.rb
@@ -4,19 +4,10 @@ module ActiveRecordUpsert
       def _upsert_record(*args)
         if self.record_timestamps
           current_time = current_time_from_proper_timezone
-          if ActiveRecord::VERSION::MAJOR >= 5 && ActiveRecord::VERSION::MINOR >= 1
-            all_timestamp_attributes_in_model.each do |column|
-              column = column.to_s
-              if has_attribute?(column) && !attribute_present?(column)
-                write_attribute(column, current_time)
-              end
-            end
-          else
-            all_timestamp_attributes.each do |column|
-              column = column.to_s
-              if has_attribute?(column) && !attribute_present?(column)
-                write_attribute(column, current_time)
-              end
+          all_timestamp_attributes_in_model.each do |column|
+            column = column.to_s
+            if has_attribute?(column) && !attribute_present?(column)
+              write_attribute(column, current_time)
             end
           end
         end
