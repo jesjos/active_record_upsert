139a9b748c5004aca18c16872479eb9a1d3647d6
Emmanuel Quentin <manu.quentin@gmail.com>
2018-04-09 18:04:55 +0200

    WIP

--- lib/active_record_upsert.rb
+++ lib/active_record_upsert.rb
@@ -11,8 +11,10 @@ end
 require 'active_record_upsert/arel'
 require 'active_record_upsert/active_record'
 
-require 'active_record_upsert/compatibility/rails51.rb' if Rails.version >= '5.1.0' && Rails.version < '5.2.0'
-require 'active_record_upsert/compatibility/rails50.rb' if Rails.version >= '5.0.0' && Rails.version < '5.1.0'
+version = defined?(Rails) ? Rails.version : ActiveRecord.version.to_s
+
+require 'active_record_upsert/compatibility/rails51.rb' if version >= '5.1.0' && version < '5.2.0'
+require 'active_record_upsert/compatibility/rails50.rb' if version >= '5.0.0' && version < '5.1.0'
 
 module ActiveRecordUpsert
   # Your code goes here...
