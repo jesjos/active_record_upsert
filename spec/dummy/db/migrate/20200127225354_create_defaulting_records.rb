c6a746e4adfbcec58cfdb3a0f8c2f87c96f85eb9
Andrew Clink <andrew@elementsmi.com>
2020-06-30 09:55:49 -0700

    Revert test migration defaults to Rails 5

--- spec/dummy/db/migrate/20200127225354_create_defaulting_records.rb
+++ spec/dummy/db/migrate/20200127225354_create_defaulting_records.rb
@@ -1,4 +1,4 @@
-class CreateDefaultingRecords < ActiveRecord::Migration[5.2]
+class CreateDefaultingRecords < ActiveRecord::Migration[5.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
