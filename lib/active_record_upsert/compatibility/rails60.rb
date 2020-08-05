3231033672c54e24061f0c79b099a0ee859a9289
Kirill Zaitsev <kirik910@gmail.com>
2020-08-05 16:25:15 +0300

    Fix query cache reset

--- lib/active_record_upsert/compatibility/rails60.rb
+++ lib/active_record_upsert/compatibility/rails60.rb
@@ -5,5 +5,14 @@ module ActiveRecordUpsert
         with_transaction_returning_status { super }
       end
     end
+
+    module ConnectAdapterExtension
+      def upsert(*args)
+        ::ActiveRecord::Base.clear_query_caches_for_current_thread
+        super
+      end
+
+      ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(self)
+    end
   end
 end
