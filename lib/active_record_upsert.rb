dacff05b15951ef262168d17a5f62703f60a4b8a
Jan Graichen <jgraichen@altimos.de>
2024-08-31 14:33:12 +0200

    fix: Load latest compat module by default
    Unless the latest compatiblity module is loaded for any newer Rails
    version, current Rails dev versions won't pass CI.
    
    Fixes #140

--- lib/active_record_upsert.rb
+++ lib/active_record_upsert.rb
@@ -13,8 +13,11 @@ require 'active_record_upsert/active_record'
 
 version = defined?(Rails) ? Rails.version : ActiveRecord.version.to_s
 
-require 'active_record_upsert/compatibility/rails70.rb' if version >= '7.0.0' && version < '8.0.0'
-require 'active_record_upsert/compatibility/rails60.rb' if version >= '6.0.0' && version < '6.2.0'
+if version >= '7.0.0'
+  require 'active_record_upsert/compatibility/rails70.rb'
+elsif version >= '6.0.0' && version < '6.2.0'
+  require 'active_record_upsert/compatibility/rails60.rb'
+end
 
 module ActiveRecordUpsert
   # Your code goes here...
